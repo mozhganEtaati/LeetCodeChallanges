7912e9cd11256ce43202ef8c3a2c5a7f4cde6074
mozhganEtaati <m.etaati73@yahoo.com>
2025-11-23 19:58:37 +0330

    first problem Merge String Alternately

---
+++ LeetCodeChallenges/LeetCodeChallenges/Problems/1768.MergeStringsAlternately/MergeStringsAlternately.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace LeetCodeChallenges.Problems._1768.MergeStringsAlternately;
+
+public class MergeStringsAlternately
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        int max = Math.Max(word1.Length, word2.Length);
+        var sb = new StringBuilder(word1.Length + word2.Length);
+
+        for (int i = 0; i < max; i++)
+        {
+            if (i < word1.Length)
+                sb.Append(word1[i]);
+
+            if (i < word2.Length)
+                sb.Append(word2[i]);
+        }
+
+        return sb.ToString();
+    }
+}
