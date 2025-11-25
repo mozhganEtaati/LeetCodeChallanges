b50d5a1550007fa41c8dcb263301f7dfb90c3daf
mozhganEtaati <m.etaati73@yahoo.com>
2025-11-25 23:06:36 +0330

    solve KidsWithCandies problem

---
+++ LeetCodeChallenges/Problems/1431. KidsWiththeGreatestNumberOfCandies/KidsWiththeGreatestNumberOfCandies.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeChallenges.Problems._1431._KidsWiththeGreatestNumberOfCandies;
+
+public static class KidsWiththeGreatestNumberOfCandies
+{
+    public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        List<bool> result = new();
+        int max = 0;
+        for (int i = 0; i < candies.Length; i++)
+        {
+            if (max < candies[i]) { max = candies[i]; }
+        }
+
+        for (int i = 0; i < candies.Length; i++)
+        {
+            result.Add(candies[i] + extraCandies >= max);
+        }
+
+        return result;
+    }
+}
