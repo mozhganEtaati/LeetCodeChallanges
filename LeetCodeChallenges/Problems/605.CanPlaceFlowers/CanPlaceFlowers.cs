cc031d3ad9de7fbc280b6062d4c581da9f1f40dd
mozhganEtaati <m.etaati73@yahoo.com>
2025-11-26 18:53:52 +0330

    solve can place flowers

---
+++ LeetCodeChallenges/Problems/605.CanPlaceFlowers/CanPlaceFlowers.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeChallenges.Problems._605.CanPlaceFlowers;
+
+public static class CanPlaceFlowers
+{
+    public static bool CanPlaceFlowersInGarden(int[] flowerbed, int n)
+    {
+        for (int i = 0; i < flowerbed.Length && n > 0; i++)
+        {
+            if (flowerbed[i] == 0)
+            {
+                bool emptyLeft = i == 0 || flowerbed[i - 1] == 0;
+                bool emptyRight = i == flowerbed.Length - 1 || flowerbed[i + 1] == 0;
+
+                if (emptyLeft && emptyRight)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+
+        }
+
+        return n == 0;
+    }
+}
