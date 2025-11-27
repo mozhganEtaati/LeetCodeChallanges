3a3a2fb0c22d44eefd39f7c56d9383b96d42eb71
mozhganEtaati <m.etaati73@yahoo.com>
2025-11-27 14:48:07 +0330

    solve ReverseVowelsOfAString

---
+++ LeetCodeChallenges/Problems/345-ReverseVowelsOfAString/ReverseVowelsOfAString.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace LeetCodeChallenges.Problems._345_ReverseVowelsOfAString;
+
+public static class ReverseVowelsOfAString
+{
+    private static readonly HashSet<char> vowelLetters = new HashSet<char>()
+    {
+        'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+    };
+
+    public static string ReverseVowels(string s)
+    {
+        var stringBuilder = new StringBuilder(s);
+        int i = 0, j = s.Length - 1;
+
+        while (i < j)
+        {
+            var isVowelFromFirst = IsVowel(stringBuilder[i]);
+            var isVowelFromLast = IsVowel(stringBuilder[j]);
+            if (!isVowelFromFirst)
+            {
+                i++;
+            }
+
+            if (!isVowelFromLast)
+            {
+                j--;
+            }
+
+            if (!isVowelFromFirst || !isVowelFromLast)
+                continue;
+
+            (stringBuilder[j], stringBuilder[i]) = (stringBuilder[i], stringBuilder[j]);
+            i++;
+            j--;
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private static bool IsVowel(char character)
+    {
+        return vowelLetters.Contains(character);
+    }
+}
