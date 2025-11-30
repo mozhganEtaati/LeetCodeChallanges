9ca9db401979d8a9967b09a494c9b4f2b693d416
mozhganEtaati <m.etaati73@yahoo.com>
2025-11-30 21:49:12 +0330

    Add 238.Product of Array Except Self solution

---
+++ LeetCodeChallenges/Problems/238.ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeChallenges.Problems._238.ProductOfArrayExceptSelf;
+
+public static class ProductOfArrayExceptSelf
+{
+    public static int[] ProductExceptSelf(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        var initial = 1;
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            result[i]=initial;
+            initial*= nums[i];
+        }
+
+        initial = 1;
+        for (int j=nums.Length-1; j>=0; j--)
+        {
+            result[j] = result[j] * initial;
+            initial *= nums[j];
+        }
+
+        return result;
+    }
+}
