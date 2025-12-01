cc99cafd0dd542064272708815b8b3dc246ba4e4
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-01 19:12:03 +0330

    solve IncreasingTripletSubsequence

---
+++ LeetCodeChallenges/Problems/334.IncreasingTripletSubsequence/IncreasingTripletSubsequence.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeChallenges.Problems._334.IncreasingTripletSubsequence;
+
+public static class IncreasingTripletSubsequence
+{
+    public static bool IncreasingTriplet(int[] nums)
+    {
+        if (nums.Length <= 2)
+            return false;
+
+        var first = int.MaxValue;
+        var mid = int.MaxValue;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (mid < nums[i])
+                return true;
+
+            if (first >= nums[i])
+                first = nums[i];
+            else
+                mid = nums[i];
+        }
+
+        return false;
+    }
+}
