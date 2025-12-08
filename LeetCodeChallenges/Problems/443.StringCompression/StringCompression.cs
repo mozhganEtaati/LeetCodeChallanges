c194b48dcc8134bc4b49cf8d080f8297217a7a25
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-08 21:26:28 +0330

    Add 443. String Compression challenge

---
+++ LeetCodeChallenges/Problems/443.StringCompression/StringCompression.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeChallenges.Problems._443.StringCompression;
+
+public static class StringCompression
+{
+    public static int Compress(char[] chars)
+    {
+        int basePointer = 0;
+
+        for (int i = 0; i < chars.Length;)
+        {
+            char current = chars[i];
+            var count = 0;
+
+            while (i < chars.Length && current == chars[i])
+            {
+                i++;
+                count++;
+            }
+
+            chars[basePointer++] = current;
+
+
+            if (count > 1)
+            {
+                foreach (char c in count.ToString())
+                {
+                    chars[basePointer++] = c;
+                }
+            }
+
+        }
+
+        return basePointer;
+    }
+}
