ce920d6557c839e3bb0a9f4686421b7194ac65d9
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-12 11:35:30 +0330

    Add solution of  392. Is Subsequence

---
+++ LeetCodeChallenges/Problems/392. IsSubsequence/IsSubsequence.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeChallenges.Problems._392._IsSubsequence;
+
+public static class IsSubsequence
+{
+    public static bool IsSubsequenceStrings(string s, string t)
+    {
+        var sPointer = 0;
+
+        for (int i = 0; i < t.Length && sPointer < s.Length; i++)
+        {
+            if (t[i] == s[sPointer])
+            {
+                sPointer++;
+            }
+        }
+
+        return sPointer == s.Length;
+    }
+}
