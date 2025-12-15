d237377da986fb736a80f0e79c4b18704b90941d
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-15 19:04:59 +0330

    solve 11. Container With Most Water

---
+++ LeetCodeChallenges/Problems/11.ContainerWithMostWater/ContainerWithMostWater.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeChallenges.Problems._11.ContainerWithMostWater;
+
+public class ContainerWithMostWater
+{
+    public static int MaxArea(int[] height)
+    {
+        var maxArea = 0;
+        var leftPointer = 0;
+        var rightPointer = height.Length - 1;
+
+        while (leftPointer < rightPointer)
+        {
+            var leftBarHeight = height[leftPointer];
+            var rightBarHeight = height[rightPointer];
+
+            var currentArea = (rightPointer - leftPointer) * (leftBarHeight < rightBarHeight ? leftBarHeight : rightBarHeight);
+
+            if (currentArea > maxArea)
+            {
+                maxArea = currentArea;
+            }
+
+            if (leftBarHeight > rightBarHeight)
+            {
+                rightPointer--;
+            }
+            else
+            {
+                leftPointer++;
+            }
+        }
+
+        return maxArea;
+    }
+}
