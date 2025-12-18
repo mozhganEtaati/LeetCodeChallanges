260fbb8737485ec1dd672b410a1c5467f82124f2
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-18 12:16:11 +0330

    Add 1679. Max Number of K-Sum Pairs solution

---
+++ LeetCodeChallenges/Problems/1679.MaxNumberofK-SumPairs/MaxNumberofK-SumPairs.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeChallenges.Problems._1679.MaxNumberofK_SumPairs;
+
+public static class MaxNumberofK_SumPairs
+{
+    public static int MaxOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        int numberOfOperations = 0;
+        int i = 0;
+        int j = nums.Length - 1;
+
+        while (i < j)
+        {
+            var sum = nums[i] + nums[j];
+            if (sum == k)
+            {
+                numberOfOperations++;
+                j--;
+                i++;
+            }
+            else if (sum < k)
+            {
+                j--;
+
+            }
+            else
+            {
+
+                i++;
+            }
+        }
+
+        return numberOfOperations;
+    }
+}
