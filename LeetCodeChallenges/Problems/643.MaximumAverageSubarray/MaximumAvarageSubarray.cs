2a85c7c4f0c05ed103cff0e08ef26faf4b2d1549
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-24 21:16:05 +0330

    Solve 643.MaximumAverageSubarray

---
+++ LeetCodeChallenges/Problems/643.MaximumAverageSubarray/MaximumAvarageSubarray.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeChallenges.Problems._643.MaximumAverageSubarray;
+
+public static class MaximumAvarageSubarray
+{
+    public static double FindMaxAverage(int[] nums, int k)
+    {
+        int windowSum = 0;
+        int i = 0;
+
+        for (; i < k; i++)
+        {
+            windowSum += nums[i];
+        }
+
+        int max = windowSum;
+
+        for (; i < nums.Length; i++)
+        {
+            windowSum += nums[i];
+            windowSum -= nums[i - k];
+            if (windowSum > max)
+                max = windowSum;
+        }
+
+        return (double)max / k;
+    }
+}
