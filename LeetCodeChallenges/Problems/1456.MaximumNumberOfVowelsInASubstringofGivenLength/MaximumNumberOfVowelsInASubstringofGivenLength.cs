5cb9ddef53d4b3ad056d3edef42b431b0bcc0fa4
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-24 21:52:19 +0330

    Solve MaxVowels challenge with slidding window

---
+++ LeetCodeChallenges/Problems/1456.MaximumNumberOfVowelsInASubstringofGivenLength/MaximumNumberOfVowelsInASubstringofGivenLength.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCodeChallenges.Problems._1456.MaximumNumberOfVowelsInASubstringofGivenLength;
+
+public static class MaximumNumberOfVowelsInASubstringofGivenLength
+{
+    public static int MaxVowels(string s, int k)
+    {
+        int maxVowelCount = 0;
+
+        int windowVowelCount = 0;
+        int i = 0;
+
+        for (; i < k; i++)
+        {
+            if (IsVowel(s[i]))
+                windowVowelCount++;
+        }
+
+        maxVowelCount = windowVowelCount;
+
+        for (; i < s.Length; i++)
+        {
+            if (IsVowel(s[i]))
+                windowVowelCount++;
+            if (IsVowel(s[i - k]))
+                windowVowelCount--;
+            if (windowVowelCount > maxVowelCount)
+                maxVowelCount = windowVowelCount;
+        }
+
+        return maxVowelCount;
+    }
+
+    public static bool IsVowel(char c)
+    {
+        switch (c)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return true;
+            default:
+                return false;
+        }
+    }
+}
