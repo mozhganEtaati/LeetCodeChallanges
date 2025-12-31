f11a98981dc1abad467b35f12d5519fae9cf56d0
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-31 21:02:39 +0330

    Add challanges

---
+++ LeetCodeChallenges/1493.LongestSubarrayof1'sAfterDeletingOneElement/LongestSubarrayof1'sAfterDeletingOneElement.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeChallenges._1493.LongestSubarrayof1_sAfterDeletingOneElement;
+
+public static class LongestSubarrayof1_sAfterDeletingOneElement
+{
+    public static int LongestSubarray(int[] nums, int k)
+    {
+        int left = 0, right = 0, max = 0, firstzeroIndex = -1;
+
+        while (right < nums.Length)
+        {
+            if (nums[right] == 0)
+            {
+                if (firstzeroIndex != -1)
+                {
+                    left = firstzeroIndex++;
+                }
+
+                firstzeroIndex = right;
+            }
+
+            var winSize = right - left + 1;
+            if (winSize > max)
+                max = winSize;
+
+            right++;
+        }
+
+        return max - 1;
+    }
+}
---
+++ LeetCodeChallenges/Problems/1004.MaxConsecutiveOnesIII/MaxConsecutiveOnesIII.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeChallenges.Problems._1004.MaxConsecutiveOnesIII;
+
+public static class MaxConsecutiveOnesIII
+{
+    public static int LongestOnes(int[] nums, int k)
+    {
+        int left = 0, right = 0, max = 0, zeroCount = 0;
+
+        while (right < nums.Length)
+        {
+            if (nums[right] == 0) zeroCount++;
+            while (zeroCount > k)
+            {
+                if (nums[left] == 0) zeroCount--;
+                left++;
+            }
+            var winSize = right - left + 1;
+            if (winSize > max)
+                max = winSize;
+
+            right++;
+        }
+
+        return max;
+    }
+}
