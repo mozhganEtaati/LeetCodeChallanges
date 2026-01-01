ca83246f20ce6d312191457f05eab522545c0fb6
mozhganEtaati <m.etaati73@yahoo.com>
2026-01-01 20:53:41 +0330

    Solve FindtheHighestAltitude challange

---
+++ LeetCodeChallenges/Problems/1732.FindtheHighestAltitude/FindtheHighestAltitude.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeChallenges.Problems._1732.FindtheHighestAltitude;
+
+public static class FindtheHighestAltitude
+{
+    public static int LargestAltitude(int[] gain)
+    {
+        int max = 0;
+        int maxAltitude = 0;
+        int i = 0;
+        while (i < gain.Length)
+        {
+            maxAltitude += gain[i];
+
+            if (maxAltitude > max)
+                max = maxAltitude;
+
+            i++;
+        }
+        return max;
+    }
+}
