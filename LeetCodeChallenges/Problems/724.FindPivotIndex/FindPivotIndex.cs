e3d42fc6f02d50738802a03c6b51680d028d2c8c
mozhganEtaati <m.etaati73@yahoo.com>
2026-01-08 12:48:56 +0330

    Solve Find Pivot index

---
+++ LeetCodeChallenges/Problems/724.FindPivotIndex/FindPivotIndex.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeChallenges.Problems._724.FindPivotIndex;
+
+public static class FindPivotIndex
+{
+    public static int PivotIndex(int[] nums)
+    {
+        int totalSum = 0;
+        foreach (var num in nums)
+            totalSum += num;
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum)
+                return i;
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
