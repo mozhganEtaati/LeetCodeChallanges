928425b97ddce3cda2e6046085dd7251e5c758b7
mozhganEtaati <m.etaati73@yahoo.com>
2026-01-08 18:00:46 +0330

    Solve UniqueNumberofOccurrences challange

---
+++ LeetCodeChallenges/Problems/1207.UniqueNumberofOccurrences/UniqueNumberofOccurrences.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeChallenges.Problems._1207.UniqueNumberofOccurrences;
+
+public static class UniqueNumberofOccurrences
+{
+    public static bool UniqueOccurrences(int[] arr)
+    {
+        var freq = new Dictionary<int, int>();
+        var seen = new HashSet<int>();
+
+        foreach (var n in arr)
+        {
+            freq[n] = freq.GetValueOrDefault(n) + 1;
+        }
+
+        foreach (var c in freq.Values)
+        {
+            if (!seen.Add(c))
+                return false;
+        }
+
+        return true;
+    }
+}
